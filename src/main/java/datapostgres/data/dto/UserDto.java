d41bd74bf95e2cc80973d50cbc0a432d2be4e065
Cris01000011 <168916184+Cris01000011@users.noreply.github.com>
2024-06-28 15:06:11 -0600

    postgres

---
+++ src/main/java/datapostgres/data/dto/UserDto.java
@@ -0,0 +1,16 @@
+package datapostgres.data.dto;
+
+public class UserDto {
+
+    private String name;
+    private String email;
+
+    public UserDto() {}
+
+    public UserDto(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and setters
+}
